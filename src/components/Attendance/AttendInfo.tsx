8154680f9f9bb658209a6d3f03a1065758ecb955
JIN921 <wlslwlsl808@gmail.com>
2025-05-25 16:20:05 +0900

    design: 출석 버튼 디자인 변경

--- src/components/Attendance/AttendInfo.tsx
+++ src/components/Attendance/AttendInfo.tsx
@@ -22,6 +22,20 @@ export const AttendInfo: React.FC<AttendInfoProps> = ({
   handleOpenModal,
   isAttend,
 }) => {
+  const getColor = () => {
+    if (isAttend) return theme.color.mainDark;
+    if (!isWithinTimeRange) return theme.color.gray[30];
+    return theme.color.main;
+  };
+
+  const getTextColor = () => {
+    return isAttend || !isWithinTimeRange
+      ? theme.color.gray[20]
+      : theme.color.gray[100];
+  };
+
+  const isDisabled = isAttend || !isWithinTimeRange;
+
   return (
     <div>
       <S.SemiBold>
@@ -36,27 +50,16 @@ export const AttendInfo: React.FC<AttendInfoProps> = ({
       </S.AttendDate>
       <S.AttendPlace>장소: {location}</S.AttendPlace>
       <S.AttendButton>
-        {isAttend ? (
-          <Button
-            width="315px"
-            height="50px"
-            color={theme.color.gray[18]}
-            textcolor={theme.color.gray[100]}
-          >
-            출석완료
-          </Button>
-        ) : (
-          <Button
-            width="315px"
-            height="50px"
-            color={theme.color.gray[30]}
-            textcolor={theme.color.gray[100]}
-            onClick={handleOpenModal}
-            disabled={!isWithinTimeRange}
-          >
-            출석하기
-          </Button>
-        )}
+        <Button
+          width="315px"
+          height="50px"
+          color={getColor()}
+          textcolor={getTextColor()}
+          disabled={isDisabled}
+          onClick={!isAttend && isWithinTimeRange ? handleOpenModal : undefined}
+        >
+          {isAttend ? '출석완료' : '출석하기'}
+        </Button>
       </S.AttendButton>
     </div>
   );
