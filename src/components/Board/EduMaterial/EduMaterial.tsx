f5aa9af006feeef0c2e53342a157358199ad4541
김지원 <jiwonkim0531@gachon.ac.kr>
2025-07-14 21:26:12 +0900

    feat: 게시판에서 교육자료 섹션 UI 구현

---
+++ src/components/Board/EduMaterial/EduMaterial.tsx
@@ -0,0 +1,38 @@
+import * as S from '@/styles/board/Board.styled';
+import { useNavigate } from 'react-router-dom';
+import Cardinal from '@/components/Board/EduMaterial/Cardinal';
+import SlideEdu from '@/components/Board/EduMaterial/SlideEdu';
+import { useGetRecentNotice } from '@/api/useGetBoardInfo';
+import Loading from '@/components/common/Loading';
+
+const EduMaterial = () => {
+  const navigate = useNavigate();
+  const handleAllEdu = () => {
+    navigate('/education');
+  };
+
+  const { recentNotices, error, recentNoticeLoading } = useGetRecentNotice();
+  if (recentNoticeLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <S.CardContainer>
+      <S.EduTitle>
+        <S.NoticeTextContainer>
+          <S.NoticeTitleText>교육자료</S.NoticeTitleText>
+          <S.AllText onClick={handleAllEdu}>전체보기 &gt;</S.AllText>
+        </S.NoticeTextContainer>
+        <S.EduAddContent>
+          참여하신 기수의 자료만 볼 수 있습니다.
+        </S.EduAddContent>
+      </S.EduTitle>
+      <Cardinal />
+      <S.ScrollContainer>
+        <SlideEdu error={error} recentNotices={recentNotices} />
+      </S.ScrollContainer>
+    </S.CardContainer>
+  );
+};
+
+export default EduMaterial;
