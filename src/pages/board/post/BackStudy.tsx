96dddc041b51d556a73f04fd4904e0442488ed9a
김지원 <jiwonkim0531@gachon.ac.kr>
2025-07-31 01:08:05 +0900

    fix: 스터디 글쓰기 페이지 템플릿 적용

--- src/pages/board/post/BackStudy.tsx
+++ src/pages/board/post/BackStudy.tsx
@@ -1,49 +1,13 @@
-import CardinalDropdown from '@/components/Board/CardinalDropdown';
-import Markdown from '@/components/Board/Markdown';
-import StudyDropdown from '@/components/Board/StudyDropdown';
-import StudyPostTitle from '@/components/Board/StudyPostTitle';
-import WeekDropdown from '@/components/Board/WeekDropdown';
-import Header from '@/components/Header/Header';
-import theme from '@/styles/theme';
-import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const InformationContainer = styled.div`
-  display: flex;
-  padding: 10px 15px;
-  gap: 5px;
-`;
-
-const DivisionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  font-size: 12px;
-  color: ${theme.color.gray[65]};
-  font-family: ${theme.font.semiBold};
-`;
-
-const DropdownContainer = styled.div`
-  display: flex;
-  gap: 5px;
-`;
-
-const MarkdownContainer = styled.div`
-  display: flex;
-  padding: 10px 15px;
-`;
+import { useState } from 'react';
+import StudyWriteTemplate from '@/components/Board/StudyWriteTemplate';
 
 const BackStudy = () => {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
   const [searchParams] = useSearchParams();
   const cardinal = searchParams.get('cardinal');
+
+  const [title, setTitle] = useState('');
   const [selectedCardinal, setSelectedCardinal] = useState<number | null>(
     Number(cardinal) || null,
   );
@@ -55,34 +19,18 @@ const BackStudy = () => {
   };
 
   return (
-    <Container>
-      <Header isAccessible RightButtonType="POST" onClickRightButton={onSave}>
-        BE 스터디
-      </Header>
-      <StudyPostTitle title={title} setTitle={setTitle} />
-      <InformationContainer>
-        <DivisionContainer>
-          구분
-          <DropdownContainer>
-            <CardinalDropdown
-              origValue={selectedCardinal}
-              editValue={setSelectedCardinal}
-            />
-            <WeekDropdown origWeek={selectedWeek} editWeek={setSelectedWeek} />
-          </DropdownContainer>
-        </DivisionContainer>
-        <DivisionContainer>
-          <DivisionContainer>스터디</DivisionContainer>
-          <StudyDropdown
-            origStudy={selectedStudy}
-            editStudy={setSelectedStudy}
-          />
-        </DivisionContainer>
-      </InformationContainer>
-      <MarkdownContainer>
-        <Markdown />
-      </MarkdownContainer>
-    </Container>
+    <StudyWriteTemplate
+      headerTitle="BE 스터디"
+      title={title}
+      setTitle={setTitle}
+      selectedCardinal={selectedCardinal}
+      setSelectedCardinal={setSelectedCardinal}
+      selectedWeek={selectedWeek}
+      setSelectedWeek={setSelectedWeek}
+      selectedStudy={selectedStudy}
+      setSelectedStudy={setSelectedStudy}
+      onSave={onSave}
+    />
   );
 };
 
