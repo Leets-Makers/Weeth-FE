21c10c1d7ea321fc039f87a5ee6c77b3783200b7
김지원 <jiwonkim0531@gachon.ac.kr>
2025-07-31 01:08:21 +0900

    fix: StudyLog type 분리

---
+++ src/types/studyLog.d.ts
@@ -0,0 +1,11 @@
+export interface StudyLog {
+  id: number;
+  name: string;
+  title: string;
+  content: string;
+  time: string;
+  commentCount: number;
+  hasFile: boolean;
+  position: string;
+  role: string;
+}
