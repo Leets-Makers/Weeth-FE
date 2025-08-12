b2bce7186a6e48cb8cc72f84ac63385f8281f16a
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-12 20:42:39 +0900

    remove: 미사용 코드 주석 처리

--- src/pages/BoardNoticeEdit.tsx
+++ src/pages/BoardNoticeEdit.tsx
@@ -1,14 +1,186 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import styled from 'styled-components';
+// import useGetBoardDetail from '@/api/useGetBoardDetail';
+// import Header from '@/components/Header/Header';
+// import FileUploader from '@/components/Board/FileUploader';
+// import PostEditor from '@/components/Board/PostEditor';
+// import PostFile from '@/components/Board/PostFile';
+// import Loading from '@/components/common/Loading';
+// import { toastError, toastInfo } from '@/components/common/ToastMessage';
+// import postBoardNotice from '@/api/postBoardNotice';
+
+// interface originFile {
+//   fileId: number;
+//   fileName: string;
+//   fileUrl: string;
+// }
+
+// const PostWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   width: 370px;
+//   padding-bottom: 20px;
+//   ul {
+//     margin: 0;
+//   }
+// `;
+
+// const FileUploaderWrapper = styled.div`
+//   margin-left: 20px;
+//   align-items: flex-start;
+//   top: calc(var(--vh, 1vh) * 40 + 120px);
+// `;
+
+// const BoardNoticeEdit = () => {
+//   const navi = useNavigate();
+//   const { postId } = useParams();
+
+//   const url = new URL(window.location.href);
+//   const pathArray = url.pathname.split('/');
+//   const path = pathArray[1];
+
+//   const [title, setTitle] = useState('');
+//   const [content, setContent] = useState('');
+//   const [files, setFiles] = useState<File[]>([]);
+//   const [originFiles, setOriginFiles] = useState<originFile[]>([]);
+//   const [loading, setLoading] = useState(false);
+
+//   const numericPostId = postId ? parseInt(postId, 10) : 0;
+
+//   const { boardDetailInfo } = useGetBoardDetail(path, numericPostId);
+
+//   useEffect(() => {
+//     setTitle(boardDetailInfo?.title ?? '');
+//     setContent(boardDetailInfo?.content ?? '');
+//     setOriginFiles(boardDetailInfo?.fileUrls ?? []);
+//   }, [boardDetailInfo]);
+
+//   const isTitleEmpty = title.trim() === '';
+//   const isContentEmpty = content.trim() === '';
+
+//   const handleDeleteOriginFile = (fileName: string) => {
+//     setOriginFiles((prevFiles) =>
+//       prevFiles.filter((file) => file.fileName !== fileName),
+//     );
+//   };
+
+//   const handleDeleteFile = (fileName: string) => {
+//     setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+//   };
+
+//   // onSave에서 파일 업로드 처리
+//   const onSave = async () => {
+//     setLoading(true);
+//     if (isTitleEmpty) {
+//       toastInfo('제목을 입력해주세요.');
+//       setLoading(false);
+//       return;
+//     }
+//     if (isContentEmpty) {
+//       toastInfo('내용을 입력해주세요.');
+//       setLoading(false);
+//       return;
+//     }
+
+//     try {
+//       // 요청 타입 결정
+//       const postType = path === 'board' ? 'editBoard' : 'editNotice';
+
+//       if (title.length > 255) {
+//         toastError('제목을 255자 이내로 작성해주세요.');
+//         return;
+//       }
+
+//       if (content.length > 65000) {
+//         toastError('내용을 65,000자 이내로 작성해주세요.');
+//         return;
+//       }
+
+//       // 서버 요청
+//       await postBoardNotice({
+//         originFiles,
+//         files,
+//         postData: {
+//           title,
+//           content,
+//           files: [],
+//         },
+//         postType,
+//         id: numericPostId,
+//       });
+
+//       // 게시글 수정 후 이동
+//       navi(path === 'board' ? '/board' : '/notice');
+//       // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     } catch (error: any) {
+//       toastError(
+//         path === 'board'
+//           ? '게시글 작성 중 문제가 발생했습니다.'
+//           : '공지사항 작성 중 문제가 발생했습니다.',
+//       );
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <PostWrapper>
+//       {loading && <Loading />}
+//       <Header
+//         onClickRightButton={onSave}
+//         RightButtonType="TEXT"
+//         isAccessible
+//         isComplete={!isTitleEmpty && !isContentEmpty}
+//       >
+//         글 쓰기
+//       </Header>
+//       <PostEditor
+//         title={title}
+//         setTitle={setTitle}
+//         content={content}
+//         setContent={setContent}
+//       />
+
+//       <FileUploaderWrapper>
+//         <FileUploader files={files} setFiles={setFiles} />
+//         {pathArray[3] === 'edit' &&
+//           originFiles.map((file) => (
+//             <PostFile
+//               key={file.fileName}
+//               fileName={file.fileName}
+//               isDownload={false}
+//               onClick={() => {
+//                 handleDeleteOriginFile(file.fileName);
+//               }}
+//             />
+//           ))}
+//         {files.length > 0 && (
+//           <>
+//             {files.map((file) => (
+//               <PostFile
+//                 key={file.name}
+//                 fileName={file.name}
+//                 isDownload={false}
+//                 onClick={() => handleDeleteFile(file.name)}
+//               />
+//             ))}
+//           </>
+//         )}
+//       </FileUploaderWrapper>
+//     </PostWrapper>
+//   );
+// };
+
+// export default BoardNoticeEdit;
+
 import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import useGetBoardDetail from '@/api/useGetBoardDetail';
-import Header from '@/components/Header/Header';
-import FileUploader from '@/components/Board/FileUploader';
-import PostEditor from '@/components/Board/PostEditor';
-import PostFile from '@/components/Board/PostFile';
-import Loading from '@/components/common/Loading';
-import { toastError, toastInfo } from '@/components/common/ToastMessage';
+import { useEffect, useState } from 'react';
 import postBoardNotice from '@/api/postBoardNotice';
+import { toastError, toastInfo } from '@/components/common/ToastMessage';
+import NoticeWrite from '@/components/Board/NoticeWrite';
+import useGetBoardDetail from '@/api/useGetBoardDetail';
 
 interface originFile {
   fileId: number;
@@ -16,25 +188,8 @@ interface originFile {
   fileUrl: string;
 }
 
-const PostWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 370px;
-  padding-bottom: 20px;
-  ul {
-    margin: 0;
-  }
-`;
-
-const FileUploaderWrapper = styled.div`
-  margin-left: 20px;
-  align-items: flex-start;
-  top: calc(var(--vh, 1vh) * 40 + 120px);
-`;
-
 const BoardNoticeEdit = () => {
-  const navi = useNavigate();
+  const navigate = useNavigate();
   const { postId } = useParams();
 
   const url = new URL(window.location.href);
@@ -45,9 +200,20 @@ const BoardNoticeEdit = () => {
   const [content, setContent] = useState('');
   const [files, setFiles] = useState<File[]>([]);
   const [originFiles, setOriginFiles] = useState<originFile[]>([]);
-  const [loading, setLoading] = useState(false);
 
+  const isTitleEmpty = title.trim() === '';
+  const isContentEmpty = content.trim() === '';
   const numericPostId = postId ? parseInt(postId, 10) : 0;
+
+  //   const handleDeleteFile = (fileName: string) => {
+  //     setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  //   };
+
+  //   const handleDeleteOriginFile = (fileName: string) => {
+  //     setOriginFiles((prevFiles) =>
+  //       prevFiles.filter((file) => file.fileName !== fileName),
+  //     );
+  //   };
 
   const { boardDetailInfo } = useGetBoardDetail(path, numericPostId);
 
@@ -57,30 +223,13 @@ const BoardNoticeEdit = () => {
     setOriginFiles(boardDetailInfo?.fileUrls ?? []);
   }, [boardDetailInfo]);
 
-  const isTitleEmpty = title.trim() === '';
-  const isContentEmpty = content.trim() === '';
-
-  const handleDeleteOriginFile = (fileName: string) => {
-    setOriginFiles((prevFiles) =>
-      prevFiles.filter((file) => file.fileName !== fileName),
-    );
-  };
-
-  const handleDeleteFile = (fileName: string) => {
-    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
-  };
-
-  // onSave에서 파일 업로드 처리
   const onSave = async () => {
-    setLoading(true);
     if (isTitleEmpty) {
       toastInfo('제목을 입력해주세요.');
-      setLoading(false);
       return;
     }
     if (isContentEmpty) {
       toastInfo('내용을 입력해주세요.');
-      setLoading(false);
       return;
     }
 
@@ -112,7 +261,7 @@ const BoardNoticeEdit = () => {
       });
 
       // 게시글 수정 후 이동
-      navi(path === 'board' ? '/board' : '/notice');
+      navigate('/board/notices');
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error: any) {
       toastError(
@@ -120,56 +269,19 @@ const BoardNoticeEdit = () => {
           ? '게시글 작성 중 문제가 발생했습니다.'
           : '공지사항 작성 중 문제가 발생했습니다.',
       );
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
-    <PostWrapper>
-      {loading && <Loading />}
-      <Header
-        onClickRightButton={onSave}
-        RightButtonType="TEXT"
-        isAccessible
-        isComplete={!isTitleEmpty && !isContentEmpty}
-      >
-        글 쓰기
-      </Header>
-      <PostEditor
-        title={title}
-        setTitle={setTitle}
-        content={content}
-        setContent={setContent}
-      />
-
-      <FileUploaderWrapper>
-        <FileUploader files={files} setFiles={setFiles} />
-        {pathArray[3] === 'edit' &&
-          originFiles.map((file) => (
-            <PostFile
-              key={file.fileName}
-              fileName={file.fileName}
-              isDownload={false}
-              onClick={() => {
-                handleDeleteOriginFile(file.fileName);
-              }}
-            />
-          ))}
-        {files.length > 0 && (
-          <>
-            {files.map((file) => (
-              <PostFile
-                key={file.name}
-                fileName={file.name}
-                isDownload={false}
-                onClick={() => handleDeleteFile(file.name)}
-              />
-            ))}
-          </>
-        )}
-      </FileUploaderWrapper>
-    </PostWrapper>
+    <NoticeWrite
+      title={title}
+      setTitle={setTitle}
+      content={content}
+      setContent={setContent}
+      files={files}
+      setFiles={setFiles}
+      onSave={onSave}
+    />
   );
 };
 
--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,145 +1,145 @@
-import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import PostListItem from '@/components/Board/PostListItem';
-import formatDate from '@/hooks/formatDate';
-import * as S from '@/styles/board/Board.styled';
-import { useGetBoardInfo } from '@/api/useGetBoardInfo';
-import Header from '@/components/Header/Header';
-import { useNavigate } from 'react-router-dom';
-import useGetGlobaluserInfo from '@/api/useGetGlobaluserInfo';
-import PostingButton from '@/components/Board/PostingButton';
-import Loading from '@/components/common/Loading';
-import useCustomBack from '@/hooks/useCustomBack';
+// import { useEffect, useRef, useState } from 'react';
+// import styled from 'styled-components';
+// import PostListItem from '@/components/Board/PostListItem';
+// import formatDate from '@/hooks/formatDate';
+// import * as S from '@/styles/board/Board.styled';
+// import { useGetBoardInfo } from '@/api/useGetBoardInfo';
+// import Header from '@/components/Header/Header';
+// import { useNavigate } from 'react-router-dom';
+// import useGetGlobaluserInfo from '@/api/useGetGlobaluserInfo';
+// import PostingButton from '@/components/Board/PostingButton';
+// import Loading from '@/components/common/Loading';
+// import useCustomBack from '@/hooks/useCustomBack';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 370px;
-  max-width: 370px;
-  margin-bottom: 50px;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 370px;
+//   max-width: 370px;
+//   margin-bottom: 50px;
+// `;
 
-const Line = styled.div`
-  border: 1px solid;
-  color: ${(props) => props.theme.color.gray[30]};
-  margin-top: 2px;
-`;
+// const Line = styled.div`
+//   border: 1px solid;
+//   color: ${(props) => props.theme.color.gray[30]};
+//   margin-top: 2px;
+// `;
 
-const PostList = styled.div`
-  margin: 0 25px 0 25px;
-`;
+// const PostList = styled.div`
+//   margin: 0 25px 0 25px;
+// `;
 
-interface Content {
-  id: number;
-  name: string;
-  title: string;
-  content: string;
-  time: string;
-  commentCount: number;
-  hasFile: boolean;
-  position: string;
-  role: string;
-}
+// interface Content {
+//   id: number;
+//   name: string;
+//   title: string;
+//   content: string;
+//   time: string;
+//   commentCount: number;
+//   hasFile: boolean;
+//   position: string;
+//   role: string;
+// }
 
-const Notice = () => {
-  useCustomBack('/board');
-  const navigate = useNavigate();
-  const { isAdmin } = useGetGlobaluserInfo();
+// const Notice = () => {
+//   useCustomBack('/board');
+//   const navigate = useNavigate();
+//   const { isAdmin } = useGetGlobaluserInfo();
 
-  const [posts, setPosts] = useState<Content[]>([]);
-  const [hasMore, setHasMore] = useState(true);
-  const [pageNumber, setPageNumber] = useState(0);
-  const [observerLoading, setObserverLoading] = useState(false);
-  const [loading, setLoading] = useState(true);
+//   const [posts, setPosts] = useState<Content[]>([]);
+//   const [hasMore, setHasMore] = useState(true);
+//   const [pageNumber, setPageNumber] = useState(0);
+//   const [observerLoading, setObserverLoading] = useState(false);
+//   const [loading, setLoading] = useState(true);
 
-  const path = 'notices';
+//   const path = 'notices';
 
-  const observerRef = useRef<HTMLDivElement | null>(null);
+//   const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const fetchData = async () => {
-    if (!observerLoading && hasMore) {
-      setObserverLoading(true);
-      await useGetBoardInfo(
-        path,
-        pageNumber,
-        setPosts,
-        setHasMore,
-        setObserverLoading,
-      );
-      setPageNumber((prevPage) => prevPage + 1);
-      setObserverLoading(false);
-      if (loading) setLoading(false);
-    }
-  };
+//   const fetchData = async () => {
+//     if (!observerLoading && hasMore) {
+//       setObserverLoading(true);
+//       await useGetBoardInfo(
+//         path,
+//         pageNumber,
+//         setPosts,
+//         setHasMore,
+//         setObserverLoading,
+//       );
+//       setPageNumber((prevPage) => prevPage + 1);
+//       setObserverLoading(false);
+//       if (loading) setLoading(false);
+//     }
+//   };
 
-  useEffect(() => {
-    // 초기 데이터 로드드
-    fetchData();
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const firstEntry = entries[0];
-        if (firstEntry.isIntersecting) {
-          // 추가 데이터 로드
-          fetchData();
-        }
-      },
-      { root: null, rootMargin: '0px', threshold: 0.1 },
-    );
+//   useEffect(() => {
+//     // 초기 데이터 로드드
+//     fetchData();
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         const firstEntry = entries[0];
+//         if (firstEntry.isIntersecting) {
+//           // 추가 데이터 로드
+//           fetchData();
+//         }
+//       },
+//       { root: null, rootMargin: '0px', threshold: 0.1 },
+//     );
 
-    if (observerRef.current) {
-      observer.observe(observerRef.current);
-    }
+//     if (observerRef.current) {
+//       observer.observe(observerRef.current);
+//     }
 
-    return () => {
-      if (observerRef.current) {
-        observer.unobserve(observerRef.current);
-      }
-    };
-  }, [hasMore, observerLoading, pageNumber]);
+//     return () => {
+//       if (observerRef.current) {
+//         observer.unobserve(observerRef.current);
+//       }
+//     };
+//   }, [hasMore, observerLoading, pageNumber]);
 
-  if (loading) {
-    return <Loading />;
-  }
+//   if (loading) {
+//     return <Loading />;
+//   }
 
-  const handlePosting = () => {
-    navigate('/notice/post');
-  };
+//   const handlePosting = () => {
+//     navigate('/notice/post');
+//   };
 
-  return (
-    <Container>
-      <Header RightButtonType="none" isAccessible={false}>
-        📢 공지사항
-      </Header>
-      {posts.map((post) => (
-        <PostList key={post.id}>
-          <PostListItem
-            name={post.name}
-            time={formatDate(post.time)}
-            title={post.title}
-            content={post.content}
-            totalComments={post.commentCount}
-            hasFile={post.hasFile}
-            position={post.position}
-            role={post.role}
-            onClick={() => navigate(`/notice/${post.id}`)}
-          />
-          <Line />
-        </PostList>
-      ))}
-      {hasMore && (
-        <div
-          ref={observerRef}
-          style={{ height: '20px', backgroundColor: 'transparent' }}
-        />
-      )}
-      {!hasMore && posts.length > 10 && <S.Text>마지막 게시물입니다.</S.Text>}
-      {isAdmin && (
-        <S.PostingButtonContainer>
-          <PostingButton onClick={handlePosting} />
-        </S.PostingButtonContainer>
-      )}
-    </Container>
-  );
-};
+//   return (
+//     <Container>
+//       <Header RightButtonType="none" isAccessible={false}>
+//         📢 공지사항
+//       </Header>
+//       {posts.map((post) => (
+//         <PostList key={post.id}>
+//           <PostListItem
+//             name={post.name}
+//             time={formatDate(post.time)}
+//             title={post.title}
+//             content={post.content}
+//             totalComments={post.commentCount}
+//             hasFile={post.hasFile}
+//             position={post.position}
+//             role={post.role}
+//             onClick={() => navigate(`/notice/${post.id}`)}
+//           />
+//           <Line />
+//         </PostList>
+//       ))}
+//       {hasMore && (
+//         <div
+//           ref={observerRef}
+//           style={{ height: '20px', backgroundColor: 'transparent' }}
+//         />
+//       )}
+//       {!hasMore && posts.length > 10 && <S.Text>마지막 게시물입니다.</S.Text>}
+//       {isAdmin && (
+//         <S.PostingButtonContainer>
+//           <PostingButton onClick={handlePosting} />
+//         </S.PostingButtonContainer>
+//       )}
+//     </Container>
+//   );
+// };
 
-export default Notice;
+// export default Notice;
