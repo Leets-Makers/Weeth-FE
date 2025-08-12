1a430d0ac3bce90893fc058eb5de5faa77f96e6e
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-12 20:12:45 +0900

    fix: api 요청 URL 경로 재설정

--- src/api/deletePost.tsx
+++ src/api/deletePost.tsx
@@ -4,9 +4,9 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 const deletePost = async (postId: number, path: string) => {
   const url =
-    path === 'board'
-      ? `${BASE_URL}/api/v1/${path}/${postId}`
-      : `${BASE_URL}/api/v1/admin/${path}/${postId}`;
+    path === 'notices'
+      ? `${BASE_URL}/api/v1/admin/${path}/${postId}`
+      : `${BASE_URL}/api/v1/board/${postId}`;
 
   return api.delete(url);
 };
