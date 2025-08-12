9f61bc8b496f12f723b7c02d9d1b68a5e88251e8
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-12 20:47:11 +0900

    fix: 요청 보낼 글쓰기 타입 재설정

--- src/types/PostRequestType.ts
+++ src/types/PostRequestType.ts
@@ -3,8 +3,15 @@ export interface CustomFiles {
   fileUrl: string;
 }
 
+type EduPart = '' | 'ALL' | 'FE' | 'BE' | 'D' | 'PM';
+
 export interface PostRequestType {
   title: string;
   content: string;
+  cardinal?: number | null;
   files: CustomFiles[];
+  category?: string;
+  studyName?: string;
+  week?: number;
+  parts?: EduPart[];
 }
