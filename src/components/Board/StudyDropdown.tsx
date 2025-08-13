85d1ca0822623966d77491d86eb5be9e6bb419d5
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-13 22:45:13 +0900

    feat: 스터디 드롭다운 api 적용

--- src/components/Board/StudyDropdown.tsx
+++ src/components/Board/StudyDropdown.tsx
@@ -1,103 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import theme from '@/styles/theme';
+import * as S from '@/styles/board/Dropdown.styled';
 import Remove from '@/assets/images/ic_remove_study.svg?react';
-
-const Container = styled.div`
-  position: relative;
-  width: 171px;
-  height: 32px;
-  box-sizing: border-box;
-  padding: 6px 10px;
-  border-radius: 10px;
-  background-color: ${theme.color.gray[18]};
-`;
-
-const Input = styled.input<{ $isNew?: boolean }>`
-  width: 100%;
-  position: relative;
-  background-color: ${theme.color.gray[18]};
-  color: ${theme.color.gray[100]};
-  font-family: ${theme.font.regular};
-  color: ${({ $isNew }) =>
-    $isNew ? theme.color.gray[65] : theme.color.gray[100]};
-  font-size: 14px;
-  border: none;
-  outline: none;
-
-  &::placeholder {
-    color: ${theme.color.gray[65]};
-  }
-
-  &:focus {
-    border: none;
-    outline: none;
-  }
-`;
-
-const DropdownList = styled.div`
-  position: absolute;
-  left: 0;
-  width: 144px;
-  max-height: 170px;
-  box-sizing: border-box;
-  background-color: ${theme.color.gray[18]};
-  border-radius: 10px;
-  padding: 5px 0px;
-  margin-top: 10px;
-  overflow-y: auto;
-  z-index: 1000;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const DropdownItem = styled.div`
-  padding: 10px;
-  height: 40px;
-  left: 10px;
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  color: white;
-  cursor: pointer;
-  font-size: 16px;
-  font-family: ${theme.font.regular};
-  // line-height: 24px;
-  // line-height: 20px;
-  // white-space: nowrap;
-
-  &:hover {
-    background-color: ${theme.color.gray[9]};
-  }
-`;
-
-// const Text = styled.div`
-//   padding-top: 8px;
-//   line-height: 20px;
-// `;
-
-const InputWrapper = styled.div`
-  display: flex;
-  position: relative;
-  gap: 10px;
-`;
-
-const ClearButton = styled.div`
-  padding-top: 3px;
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
-
-const HiddenText = styled.span`
-  position: absolute;
-  visibility: hidden;
-  white-space: pre;
-  font-size: 14px;
-  font-family: ${theme.font.regular};
-  color: ${theme.color.gray[65]};
-`;
+import { useParams } from 'react-router-dom';
+import { RealPart } from '@/types/part';
+import getStudyLists from '@/api/useGetStudyList';
+import { toastError } from '../common/ToastMessage';
 
 interface Props {
   origStudy: string | null;
@@ -107,15 +14,25 @@ interface Props {
 const StudyDropdown = ({ origStudy, editStudy }: Props) => {
   const dropdownRef = useRef<HTMLDivElement>(null);
   const [inputValue, setInputValue] = useState('');
-  const [studyList, setStudyList] = useState<string[]>([
-    'React',
-    '데이터분석',
-    '자바스크립트',
-    '스프링',
-  ]);
+  const [studyList, setStudyList] = useState<string[]>([]);
   const [isOpen, setIsOpen] = useState(false);
   const textWidthRef = useRef<HTMLSpanElement>(null);
   const [textWidth, setTextWidth] = useState(0);
+  const { part } = useParams<{ part: RealPart }>();
+
+  useEffect(() => {
+    if (!part) return;
+
+    (async () => {
+      try {
+        const names = await getStudyLists(part);
+        setStudyList(names);
+      } catch (e) {
+        toastError('스터디 목록을 불러오지 못했습니다.');
+        console.error(e);
+      }
+    })();
+  }, [part]);
 
   const isInList = studyList.some(
     (s) => s.toLowerCase() === inputValue.trim().toLowerCase(),
@@ -171,9 +88,9 @@ const StudyDropdown = ({ origStudy, editStudy }: Props) => {
   }, []);
 
   return (
-    <Container ref={dropdownRef}>
-      <InputWrapper>
-        <Input
+    <S.Container ref={dropdownRef}>
+      <S.InputWrapper>
+        <S.Input
           value={inputValue}
           placeholder="옵션 검색"
           onFocus={() => setIsOpen(true)}
@@ -189,34 +106,34 @@ const StudyDropdown = ({ origStudy, editStudy }: Props) => {
             }
           }}
         />
-        <HiddenText ref={textWidthRef}>{inputValue}</HiddenText>
+        <S.HiddenText ref={textWidthRef}>{inputValue}</S.HiddenText>
 
         {isNew && (
-          <ClearButton
+          <S.ClearButton
             onMouseDown={(e) => e.preventDefault()}
             onClick={handleClear}
             style={{ left: textWidth + 10 }}
             aria-label="입력 지우기"
           >
             <Remove />
-          </ClearButton>
+          </S.ClearButton>
         )}
-      </InputWrapper>
+      </S.InputWrapper>
       {isOpen && (
-        <DropdownList>
+        <S.StudyDropdownList>
           {filteredList.map((item) => (
-            <DropdownItem key={item} onClick={() => handleSelect(item)}>
+            <S.StudyDropdownItem key={item} onClick={() => handleSelect(item)}>
               {item}
-            </DropdownItem>
+            </S.StudyDropdownItem>
           ))}
           {showAddOption && (
-            <DropdownItem onClick={handleAddOption}>
-              `{inputValue}` 추가하기
-            </DropdownItem>
+            <S.StudyDropdownItem onClick={handleAddOption}>
+              &apos;{inputValue}&apos; 추가하기
+            </S.StudyDropdownItem>
           )}
-        </DropdownList>
+        </S.StudyDropdownList>
       )}
-    </Container>
+    </S.Container>
   );
 };
 
