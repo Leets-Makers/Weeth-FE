ad0736d9956ccc07650de53f6193e86e474aa7ed
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-13 22:59:22 +0900

    remove: 미사용 코드 주석 처리

--- src/pages/BoardDetail.tsx
+++ src/pages/BoardDetail.tsx
@@ -1,237 +1,237 @@
-import { useState } from 'react';
-import useGetBoardDetail from '@/api/useGetBoardDetail';
-import CommentInput from '@/components/Board/CommentInput';
-import PostCommentList from '@/components/Board/PostCommentList';
-import PostDetailMain from '@/components/Board/PostDetailMain';
-import Header from '@/components/Header/Header';
-import styled from 'styled-components';
-import { useNavigate, useParams } from 'react-router-dom';
-import useGetUserName from '@/hooks/useGetUserName';
-import deletePost from '@/api/deletePost';
-import MenuModal from '@/components/common/MenuModal';
-import theme from '@/styles/theme';
-import { toastInfo, toastError } from '@/components/common/ToastMessage';
-import SelectModal from '@/components/Modal/SelectModal';
-import Loading from '@/components/common/Loading';
+// import { useState } from 'react';
+// import useGetBoardDetail from '@/api/useGetBoardDetail';
+// import CommentInput from '@/components/Board/CommentInput';
+// import PostCommentList from '@/components/Board/PostCommentList';
+// import PostDetailMain from '@/components/Board/PostDetailMain';
+// import Header from '@/components/Header/Header';
+// import styled from 'styled-components';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import useGetUserName from '@/hooks/useGetUserName';
+// import deletePost from '@/api/deletePost';
+// import MenuModal from '@/components/common/MenuModal';
+// import theme from '@/styles/theme';
+// import { toastInfo, toastError } from '@/components/common/ToastMessage';
+// import SelectModal from '@/components/Modal/SelectModal';
+// import Loading from '@/components/common/Loading';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 370px;
-  margin: 0 auto;
-  padding-bottom: 60px;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+//   max-width: 370px;
+//   margin: 0 auto;
+//   padding-bottom: 60px;
+// `;
 
-const CommentInputContainer = styled.div`
-  position: fixed;
-  bottom: 10px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100%;
-  max-width: 370px;
-  z-index: 10;
-  padding: 10px;
-  display: flex;
-  justify-content: center;
-`;
+// const CommentInputContainer = styled.div`
+//   position: fixed;
+//   bottom: 10px;
+//   left: 50%;
+//   transform: translateX(-50%);
+//   width: 100%;
+//   max-width: 370px;
+//   z-index: 10;
+//   padding: 10px;
+//   display: flex;
+//   justify-content: center;
+// `;
 
-const TextButton = styled.div<{ $isLast?: boolean }>`
-  width: calc(100% - 8px);
-  box-sizing: border-box;
-  padding: 12px 0 12px 16px;
-  margin: 0 4px;
-  border-bottom: ${(props) =>
-    props.$isLast ? 'none' : `1px solid ${theme.color.gray[30]}`};
-  color: ${(props) => (props.$isLast ? theme.color.negative : 'white')};
-`;
+// const TextButton = styled.div<{ $isLast?: boolean }>`
+//   width: calc(100% - 8px);
+//   box-sizing: border-box;
+//   padding: 12px 0 12px 16px;
+//   margin: 0 4px;
+//   border-bottom: ${(props) =>
+//     props.$isLast ? 'none' : `1px solid ${theme.color.gray[30]}`};
+//   color: ${(props) => (props.$isLast ? theme.color.negative : 'white')};
+// `;
 
-const BoardDetail = () => {
-  const { category, part, postId } = useParams<{
-    category: string;
-    part: string;
-    postId: string;
-  }>();
+// const BoardDetail = () => {
+//   const { category, part, postId } = useParams<{
+//     category: string;
+//     part: string;
+//     postId: string;
+//   }>();
 
-  // postId를 숫자로 변환
-  const numericPostId = postId ? parseInt(postId, 10) : null;
+//   // postId를 숫자로 변환
+//   const numericPostId = postId ? parseInt(postId, 10) : null;
 
-  // postId가 유효하지 않으면 에러 처리
-  if (!numericPostId) {
-    return <div>잘못된 게시물 ID입니다.</div>;
-  }
+//   // postId가 유효하지 않으면 에러 처리
+//   if (!numericPostId) {
+//     return <div>잘못된 게시물 ID입니다.</div>;
+//   }
 
-  const [refreshKey, setRefreshKey] = useState(0);
+//   const [refreshKey, setRefreshKey] = useState(0);
 
-  // 대댓글 작성시 부모 댓글 ID 상태
-  const [parentCommentId, setParentCommentId] = useState<number | null>(null);
+//   // 대댓글 작성시 부모 댓글 ID 상태
+//   const [parentCommentId, setParentCommentId] = useState<number | null>(null);
 
-  if (!part || !postId) {
-    return <div>잘못된 경로입니다.</div>;
-  }
+//   if (!part || !postId) {
+//     return <div>잘못된 경로입니다.</div>;
+//   }
 
-  // refreshKey를 의존성으로 사용
-  const { boardDetailInfo, error, loading } = useGetBoardDetail(
-    part,
-    numericPostId,
-    refreshKey,
-  );
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isSelectModalOpen, setIsSelectModalOpen] = useState(false);
+//   // refreshKey를 의존성으로 사용
+//   const { boardDetailInfo, error, loading } = useGetBoardDetail(
+//     part,
+//     numericPostId,
+//     refreshKey,
+//   );
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [isSelectModalOpen, setIsSelectModalOpen] = useState(false);
 
-  // 대댓글 작성시 본댓글 하이라이팅
-  const [selectedComment, setSelectedComment] = useState<
-    Record<number, boolean>
-  >({});
+//   // 대댓글 작성시 본댓글 하이라이팅
+//   const [selectedComment, setSelectedComment] = useState<
+//     Record<number, boolean>
+//   >({});
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  const openSelectModal = () => {
-    setIsSelectModalOpen(true);
-  };
+//   const openSelectModal = () => {
+//     setIsSelectModalOpen(true);
+//   };
 
-  const closeSelectModal = () => {
-    setIsSelectModalOpen(false);
-  };
+//   const closeSelectModal = () => {
+//     setIsSelectModalOpen(false);
+//   };
 
-  const confirmDelete = async () => {
-    try {
-      await deletePost(numericPostId, part);
-      navigate(`/board/${category}/${part}`, { replace: true });
-      setTimeout(() => {
-        toastInfo('게시물이 삭제되었습니다');
-      }, 500);
-    } catch (err) {
-      toastError();
-      console.error(err);
-    }
-    closeSelectModal();
-  };
+//   const confirmDelete = async () => {
+//     try {
+//       await deletePost(numericPostId, part);
+//       navigate(`/board/${category}/${part}`, { replace: true });
+//       setTimeout(() => {
+//         toastInfo('게시물이 삭제되었습니다');
+//       }, 500);
+//     } catch (err) {
+//       toastError();
+//       console.error(err);
+//     }
+//     closeSelectModal();
+//   };
 
-  const handleRefresh = () => {
-    setRefreshKey((prev) => prev + 1);
-  };
+//   const handleRefresh = () => {
+//     setRefreshKey((prev) => prev + 1);
+//   };
 
-  const handleCommentSuccess = () => {
-    setTimeout(() => {
-      setParentCommentId(null);
-      setSelectedComment({});
-    }, 200);
-    handleRefresh();
-  };
+//   const handleCommentSuccess = () => {
+//     setTimeout(() => {
+//       setParentCommentId(null);
+//       setSelectedComment({});
+//     }, 200);
+//     handleRefresh();
+//   };
 
-  const handleReply = (commentId: number) => {
-    setParentCommentId(commentId);
-    setSelectedComment((prev) => ({
-      ...prev,
-      [commentId]: !prev[commentId],
-    }));
-  };
+//   const handleReply = (commentId: number) => {
+//     setParentCommentId(commentId);
+//     setSelectedComment((prev) => ({
+//       ...prev,
+//       [commentId]: !prev[commentId],
+//     }));
+//   };
 
-  const isMyPost = boardDetailInfo?.name === useGetUserName();
+//   const isMyPost = boardDetailInfo?.name === useGetUserName();
 
-  const getHeaderTitle = () => {
-    if (category === 'study') {
-      const studyMap: Record<string, string> = {
-        ALL: '전체 스터디',
-        PM: 'PM 스터디',
-        FE: 'FE 스터디',
-        BE: 'BE 스터디',
-        D: 'D 스터디',
-      };
-      return studyMap[part] || '스터디 게시판';
-    }
+//   const getHeaderTitle = () => {
+//     if (category === 'study') {
+//       const studyMap: Record<string, string> = {
+//         ALL: '전체 스터디',
+//         PM: 'PM 스터디',
+//         FE: 'FE 스터디',
+//         BE: 'BE 스터디',
+//         D: 'D 스터디',
+//       };
+//       return studyMap[part] || '스터디 게시판';
+//     }
 
-    if (category === 'article') {
-      const articleMap: Record<string, string> = {
-        ALL: '전체 아티클',
-        PM: 'PM 아티클',
-        FE: 'FE 아티클',
-        BE: 'BE 아티클',
-        D: 'D 아티클',
-      };
-      return articleMap[part] || '아티클 게시판';
-    }
+//     if (category === 'article') {
+//       const articleMap: Record<string, string> = {
+//         ALL: '전체 아티클',
+//         PM: 'PM 아티클',
+//         FE: 'FE 아티클',
+//         BE: 'BE 아티클',
+//         D: 'D 아티클',
+//       };
+//       return articleMap[part] || '아티클 게시판';
+//     }
 
-    if (category === 'education') {
-      return '교육자료';
-    }
+//     if (category === 'education') {
+//       return '교육자료';
+//     }
 
-    if (category === 'notices') {
-      return '공지사항';
-    }
+//     if (category === 'notices') {
+//       return '공지사항';
+//     }
 
-    return '게시판';
-  };
+//     return '게시판';
+//   };
 
-  if (error) return <div>오류: {error}</div>;
+//   if (error) return <div>오류: {error}</div>;
 
-  if (loading) return <Loading />;
+//   if (loading) return <Loading />;
 
-  return (
-    <>
-      {isModalOpen && (
-        <MenuModal
-          onClose={() => {
-            setIsModalOpen(false);
-          }}
-        >
-          <TextButton
-            onClick={() => {
-              navigate(`/board/${postId}/edit`);
-            }}
-          >
-            수정
-          </TextButton>
-          <TextButton $isLast onClick={openSelectModal}>
-            삭제
-          </TextButton>
-        </MenuModal>
-      )}
-      {isSelectModalOpen && (
-        <SelectModal
-          title="게시물 삭제"
-          content="이 게시물을 정말 삭제하시겠습니까?"
-          onClose={closeSelectModal}
-          onDelete={confirmDelete}
-        />
-      )}
-      <Container>
-        <Header
-          RightButtonType="MENU"
-          isAccessible={isMyPost}
-          onClickRightButton={() => {
-            setIsModalOpen(true);
-          }}
-        >
-          {getHeaderTitle()}
-        </Header>
+//   return (
+//     <>
+//       {isModalOpen && (
+//         <MenuModal
+//           onClose={() => {
+//             setIsModalOpen(false);
+//           }}
+//         >
+//           <TextButton
+//             onClick={() => {
+//               navigate(`/board/${postId}/edit`);
+//             }}
+//           >
+//             수정
+//           </TextButton>
+//           <TextButton $isLast onClick={openSelectModal}>
+//             삭제
+//           </TextButton>
+//         </MenuModal>
+//       )}
+//       {isSelectModalOpen && (
+//         <SelectModal
+//           title="게시물 삭제"
+//           content="이 게시물을 정말 삭제하시겠습니까?"
+//           onClose={closeSelectModal}
+//           onDelete={confirmDelete}
+//         />
+//       )}
+//       <Container>
+//         <Header
+//           RightButtonType="MENU"
+//           isAccessible={isMyPost}
+//           onClickRightButton={() => {
+//             setIsModalOpen(true);
+//           }}
+//         >
+//           {getHeaderTitle()}
+//         </Header>
 
-        {boardDetailInfo && (
-          <>
-            <PostDetailMain info={boardDetailInfo} />
-            <PostCommentList
-              comments={boardDetailInfo.comments}
-              postId={boardDetailInfo.id}
-              path={part}
-              onCommentDelete={handleRefresh}
-              onReply={handleReply}
-              selectedComment={selectedComment}
-            />
-          </>
-        )}
-      </Container>
-      <CommentInputContainer>
-        {boardDetailInfo && (
-          <CommentInput
-            postId={boardDetailInfo.id}
-            initialParentCommentId={parentCommentId}
-            onCommentSuccess={handleCommentSuccess}
-          />
-        )}
-      </CommentInputContainer>
-    </>
-  );
-};
+//         {boardDetailInfo && (
+//           <>
+//             <PostDetailMain info={boardDetailInfo} />
+//             <PostCommentList
+//               comments={boardDetailInfo.comments}
+//               postId={boardDetailInfo.id}
+//               path={part}
+//               onCommentDelete={handleRefresh}
+//               onReply={handleReply}
+//               selectedComment={selectedComment}
+//             />
+//           </>
+//         )}
+//       </Container>
+//       <CommentInputContainer>
+//         {boardDetailInfo && (
+//           <CommentInput
+//             postId={boardDetailInfo.id}
+//             initialParentCommentId={parentCommentId}
+//             onCommentSuccess={handleCommentSuccess}
+//           />
+//         )}
+//       </CommentInputContainer>
+//     </>
+//   );
+// };
 
-export default BoardDetail;
+// export default BoardDetail;
--- src/pages/BoardNoticeEdit.tsx
+++ src/pages/BoardNoticeEdit.tsx
@@ -1,14 +1,9 @@
-// import { useEffect, useState } from 'react';
 // import { useNavigate, useParams } from 'react-router-dom';
-// import styled from 'styled-components';
-// import useGetBoardDetail from '@/api/useGetBoardDetail';
-// import Header from '@/components/Header/Header';
-// import FileUploader from '@/components/Board/FileUploader';
-// import PostEditor from '@/components/Board/PostEditor';
-// import PostFile from '@/components/Board/PostFile';
-// import Loading from '@/components/common/Loading';
-// import { toastError, toastInfo } from '@/components/common/ToastMessage';
+// import { useEffect, useState } from 'react';
 // import postBoardNotice from '@/api/postBoardNotice';
+// import { toastError, toastInfo } from '@/components/common/ToastMessage';
+// import NoticeWrite from '@/components/Board/NoticeWrite';
+// import useGetBoardDetail from '@/api/useGetBoardDetail';
 
 // interface originFile {
 //   fileId: number;
@@ -16,25 +11,8 @@
 //   fileUrl: string;
 // }
 
-// const PostWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   width: 370px;
-//   padding-bottom: 20px;
-//   ul {
-//     margin: 0;
-//   }
-// `;
-
-// const FileUploaderWrapper = styled.div`
-//   margin-left: 20px;
-//   align-items: flex-start;
-//   top: calc(var(--vh, 1vh) * 40 + 120px);
-// `;
-
 // const BoardNoticeEdit = () => {
-//   const navi = useNavigate();
+//   const navigate = useNavigate();
 //   const { postId } = useParams();
 
 //   const url = new URL(window.location.href);
@@ -45,8 +23,9 @@
 //   const [content, setContent] = useState('');
 //   const [files, setFiles] = useState<File[]>([]);
 //   const [originFiles, setOriginFiles] = useState<originFile[]>([]);
-//   const [loading, setLoading] = useState(false);
 
+//   const isTitleEmpty = title.trim() === '';
+//   const isContentEmpty = content.trim() === '';
 //   const numericPostId = postId ? parseInt(postId, 10) : 0;
 
 //   const { boardDetailInfo } = useGetBoardDetail(path, numericPostId);
@@ -57,30 +36,13 @@
 //     setOriginFiles(boardDetailInfo?.fileUrls ?? []);
 //   }, [boardDetailInfo]);
 
-//   const isTitleEmpty = title.trim() === '';
-//   const isContentEmpty = content.trim() === '';
-
-//   const handleDeleteOriginFile = (fileName: string) => {
-//     setOriginFiles((prevFiles) =>
-//       prevFiles.filter((file) => file.fileName !== fileName),
-//     );
-//   };
-
-//   const handleDeleteFile = (fileName: string) => {
-//     setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
-//   };
-
-//   // onSave에서 파일 업로드 처리
 //   const onSave = async () => {
-//     setLoading(true);
 //     if (isTitleEmpty) {
 //       toastInfo('제목을 입력해주세요.');
-//       setLoading(false);
 //       return;
 //     }
 //     if (isContentEmpty) {
 //       toastInfo('내용을 입력해주세요.');
-//       setLoading(false);
 //       return;
 //     }
 
@@ -112,7 +74,7 @@
 //       });
 
 //       // 게시글 수정 후 이동
-//       navi(path === 'board' ? '/board' : '/notice');
+//       navigate('/board/notices');
 //       // eslint-disable-next-line @typescript-eslint/no-unused-vars
 //     } catch (error: any) {
 //       toastError(
@@ -120,169 +82,20 @@
 //           ? '게시글 작성 중 문제가 발생했습니다.'
 //           : '공지사항 작성 중 문제가 발생했습니다.',
 //       );
-//     } finally {
-//       setLoading(false);
 //     }
 //   };
 
 //   return (
-//     <PostWrapper>
-//       {loading && <Loading />}
-//       <Header
-//         onClickRightButton={onSave}
-//         RightButtonType="TEXT"
-//         isAccessible
-//         isComplete={!isTitleEmpty && !isContentEmpty}
-//       >
-//         글 쓰기
-//       </Header>
-//       <PostEditor
-//         title={title}
-//         setTitle={setTitle}
-//         content={content}
-//         setContent={setContent}
-//       />
-
-//       <FileUploaderWrapper>
-//         <FileUploader files={files} setFiles={setFiles} />
-//         {pathArray[3] === 'edit' &&
-//           originFiles.map((file) => (
-//             <PostFile
-//               key={file.fileName}
-//               fileName={file.fileName}
-//               isDownload={false}
-//               onClick={() => {
-//                 handleDeleteOriginFile(file.fileName);
-//               }}
-//             />
-//           ))}
-//         {files.length > 0 && (
-//           <>
-//             {files.map((file) => (
-//               <PostFile
-//                 key={file.name}
-//                 fileName={file.name}
-//                 isDownload={false}
-//                 onClick={() => handleDeleteFile(file.name)}
-//               />
-//             ))}
-//           </>
-//         )}
-//       </FileUploaderWrapper>
-//     </PostWrapper>
+//     <NoticeWrite
+//       title={title}
+//       setTitle={setTitle}
+//       content={content}
+//       setContent={setContent}
+//       files={files}
+//       setFiles={setFiles}
+//       onSave={onSave}
+//     />
 //   );
 // };
 
 // export default BoardNoticeEdit;
-
-import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import postBoardNotice from '@/api/postBoardNotice';
-import { toastError, toastInfo } from '@/components/common/ToastMessage';
-import NoticeWrite from '@/components/Board/NoticeWrite';
-import useGetBoardDetail from '@/api/useGetBoardDetail';
-
-interface originFile {
-  fileId: number;
-  fileName: string;
-  fileUrl: string;
-}
-
-const BoardNoticeEdit = () => {
-  const navigate = useNavigate();
-  const { postId } = useParams();
-
-  const url = new URL(window.location.href);
-  const pathArray = url.pathname.split('/');
-  const path = pathArray[1];
-
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [files, setFiles] = useState<File[]>([]);
-  const [originFiles, setOriginFiles] = useState<originFile[]>([]);
-
-  const isTitleEmpty = title.trim() === '';
-  const isContentEmpty = content.trim() === '';
-  const numericPostId = postId ? parseInt(postId, 10) : 0;
-
-  //   const handleDeleteFile = (fileName: string) => {
-  //     setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
-  //   };
-
-  //   const handleDeleteOriginFile = (fileName: string) => {
-  //     setOriginFiles((prevFiles) =>
-  //       prevFiles.filter((file) => file.fileName !== fileName),
-  //     );
-  //   };
-
-  const { boardDetailInfo } = useGetBoardDetail(path, numericPostId);
-
-  useEffect(() => {
-    setTitle(boardDetailInfo?.title ?? '');
-    setContent(boardDetailInfo?.content ?? '');
-    setOriginFiles(boardDetailInfo?.fileUrls ?? []);
-  }, [boardDetailInfo]);
-
-  const onSave = async () => {
-    if (isTitleEmpty) {
-      toastInfo('제목을 입력해주세요.');
-      return;
-    }
-    if (isContentEmpty) {
-      toastInfo('내용을 입력해주세요.');
-      return;
-    }
-
-    try {
-      // 요청 타입 결정
-      const postType = path === 'board' ? 'editBoard' : 'editNotice';
-
-      if (title.length > 255) {
-        toastError('제목을 255자 이내로 작성해주세요.');
-        return;
-      }
-
-      if (content.length > 65000) {
-        toastError('내용을 65,000자 이내로 작성해주세요.');
-        return;
-      }
-
-      // 서버 요청
-      await postBoardNotice({
-        originFiles,
-        files,
-        postData: {
-          title,
-          content,
-          files: [],
-        },
-        postType,
-        id: numericPostId,
-      });
-
-      // 게시글 수정 후 이동
-      navigate('/board/notices');
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error: any) {
-      toastError(
-        path === 'board'
-          ? '게시글 작성 중 문제가 발생했습니다.'
-          : '공지사항 작성 중 문제가 발생했습니다.',
-      );
-    }
-  };
-
-  return (
-    <NoticeWrite
-      title={title}
-      setTitle={setTitle}
-      content={content}
-      setContent={setContent}
-      files={files}
-      setFiles={setFiles}
-      onSave={onSave}
-    />
-  );
-};
-
-export default BoardNoticeEdit;
