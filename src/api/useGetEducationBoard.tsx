e57faf46c0bed9ab16792007098a19d96676c70f
김지원 <jiwonkim0531@gachon.ac.kr>
2025-08-14 15:40:40 +0900

    feat: 해당 api 응답 타입 분리

--- src/api/useGetEducationBoard.tsx
+++ src/api/useGetEducationBoard.tsx
@@ -1,57 +1,6 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import api from './api';
-
-type Part = 'ALL' | 'FE' | 'BE' | 'D' | 'PM';
-
-interface PartEduBoardQuery {
-  part: Part;
-  cardinalNumber: number | undefined;
-  pageNumber: number;
-  pageSize: number;
-}
-
-export interface PartEduContent {
-  id: number;
-  name: string;
-  parts: Part[];
-  position: string;
-  role: string;
-  title: string;
-  content: string;
-  studyName: string;
-  week: number;
-  time: string;
-  commentCount: number;
-  hasFile: boolean;
-  isNew: boolean;
-}
-
-interface PartBoardSort {
-  empty: boolean;
-  unsorted: boolean;
-  sorted: boolean;
-}
-
-interface Pageable {
-  offset: number;
-  sort: PartBoardSort;
-  unpaged: boolean;
-  pageNumber: number;
-  pageSize: number;
-  paged: boolean;
-}
-
-interface GetEduBoardResponse {
-  size: number;
-  content: PartEduContent[];
-  number: number;
-  sort: PartBoardSort;
-  pageable: Pageable;
-  numberOfElements: number;
-  first: boolean;
-  last: boolean;
-  empty: boolean;
-}
+import api from '@/api/api';
+import { GetEduBoardResponse, PartEduBoardQuery } from '@/types/education';
 
 const fetchPartBoard = async (
   params: PartEduBoardQuery,
--- src/api/useGetPartBoard.tsx
+++ src/api/useGetPartBoard.tsx
@@ -1,56 +1,6 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import api from './api';
-
-interface PartBoardQuery {
-  part: string;
-  category: string;
-  cardinalNumber?: number;
-  week?: number;
-  studyName?: string;
-  pageNumber: number;
-  pageSize: number;
-}
-
-interface PartBoardContent {
-  id: number;
-  name: string;
-  position: string;
-  role: string;
-  title: string;
-  content: string;
-  studyName: string;
-  week: number;
-  time: string;
-  commentCount: number;
-  hasFile: boolean;
-  isNew: boolean;
-}
-
-interface PartBoardSort {
-  empty: boolean;
-  unsorted: boolean;
-  sorted: boolean;
-}
-
-interface Pageable {
-  offset: number;
-  sort: PartBoardSort;
-  unpaged: boolean;
-  pageNumber: number;
-  pageSize: number;
-  paged: boolean;
-}
-
-interface GetPartBoardResponse {
-  size: number;
-  content: PartBoardContent[];
-  sort: PartBoardSort;
-  pageable: Pageable;
-  numberOfElements: number;
-  first: boolean;
-  last: boolean;
-  empty: boolean;
-}
+import api from '@/api/api';
+import { GetPartBoardResponse, PartBoardQuery } from '@/types/partBoard';
 
 const fetchPartBoard = async (
   params: PartBoardQuery,
