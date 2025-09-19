561dfdc9533aef1c10ee1287739bc97bbd649079
김지원 <jiwonkim0531@gachon.ac.kr>
2025-09-19 14:48:19 +0900

    fix: 출석 페이지 헤더에서 관리자 헤더로 수정

--- src/pages/attend/Attendance.tsx
+++ src/pages/attend/Attendance.tsx
@@ -1,7 +1,9 @@
+import useGetUserInfo from '@/api/useGetGlobaluserInfo';
 import AttendMain from '@/components/Attendance/AttendMain';
 import Header from '@/components/Header/Header';
 import useCustomBack from '@/hooks/useCustomBack';
 import { MOBILE } from '@/styles';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -14,10 +16,20 @@ const Container = styled.div`
 
 const Attendance: React.FC = () => {
   useCustomBack('/home');
+  const { isAdmin } = useGetUserInfo();
+  const nav = useNavigate();
+
+  const handleRightButton = () => {
+    nav(`/admin/attendance`);
+  };
 
   return (
     <Container>
-      <Header RightButtonType="none" isAccessible>
+      <Header
+        isAccessible={isAdmin}
+        RightButtonType="ADMIN"
+        onClickRightButton={handleRightButton}
+      >
         출석
       </Header>
       <AttendMain />
