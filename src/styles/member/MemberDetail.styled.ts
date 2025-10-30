d1dce340ef9bf617e725e05f56e0c5e2973a6fee
JIN921 <wlslwlsl808@gmail.com>
2025-10-30 16:34:49 +0900

    refactor: 멤버 컴포넌트 스타일 분리

---
+++ src/styles/member/MemberDetail.styled.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 370px;
+`;
+
+export const PositionCharacter = styled.img`
+  margin-top: 52px;
+  width: 14.3rem;
+`;
+
+export const ClipContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Clip = styled.img`
+  position: absolute;
+  top: -10px;
+  left: 20px;
+`;
+
+export const ContentTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.color.gray[18]};
+  gap: 20px;
+  width: 350px;
+  height: 136px;
+  margin-top: 35px;
+  border-radius: 19px 19px 0 0;
+  padding: 16px;
+  box-sizing: border-box;
+`;
+
+export const ContentBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 350px;
+  border: 1px solid ${theme.color.gray[18]};
+  border-radius: 0 0 19px 19px;
+  padding: 17px 14px 34px;
+  box-sizing: border-box;
+`;
+
+export const CardinalList = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+export const InfoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Position = styled.div<{ color: string }>`
+  color: ${({ color }) => color};
+  font-family: ${theme.font.semiBold};
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 32px;
+  font-family: ${theme.font.semiBold};
+`;
+
+export const Department = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const Gray = styled.div`
+  color: ${theme.color.gray[65]};
+`;
+
+export const ErrorMessage = styled.div`
+  color: ${theme.color.gray[65]};
+  margin-top: 50px;
+  font-size: 16px;
+`;
