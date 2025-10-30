93d4206679766b0143f1c0107089bec24a36e7a3
Dahyeon <dahhyeon22@gmail.com>
2025-10-30 18:19:20 +0900

    file: 불필요한 app.css 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,12 @@ import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { CustomToastContainer } from '@/components/common/ToastMessage';
 
-import ScrollToTop from '@/hooks/ScrollToTop';
 import theme from '@/styles/theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CustomToastContainer limit={1} />
-      {/* <ScrollToTop /> */}
       <RouterProvider router={router} />
     </ThemeProvider>
   );
