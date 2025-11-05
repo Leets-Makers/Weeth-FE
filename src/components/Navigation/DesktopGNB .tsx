fadebdecd77583cf297f42b54ce6b8ed14ea5ef7
JIN921 <wlslwlsl808@gmail.com>
2025-11-05 16:06:06 +0900

    design: 데스크탑 GNB 스타일 설정 완료

--- src/components/Navigation/DesktopGNB .tsx
+++ src/components/Navigation/DesktopGNB .tsx
@@ -3,6 +3,7 @@ import logo from '@/assets/images/ic_name_logo.svg';
 import useGetUserInfo from '@/api/useGetUserInfo';
 import useSetPosition from '@/hooks/useSetPosition';
 import { colors, units } from '@/theme/designTokens';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   display: flex;
@@ -21,7 +22,7 @@ const Left = styled.div`
 
 const Logo = styled.img`
   height: 40px;
-  margin-left: 16px;
+  margin-right: 16px;
 `;
 
 const Menu = styled.ul`
@@ -29,34 +30,64 @@ const Menu = styled.ul`
   gap: 16px;
   color: ${colors.light.neutral[0]};
   list-style: none;
-  padding: 0;
+  padding: 5px 0 0 0;
+`;
+
+const MenuItem = styled.li<{ active?: boolean }>`
+  cursor: pointer;
+  font-weight: ${({ active }) => (active ? 600 : 400)};
+  color: ${({ active }) =>
+    active ? colors.light.primary[500] : colors.light.neutral[0]};
+  transition: color 0.2s;
+
+  &:hover {
+    color: ${colors.light.primary[500]};
+  }
 `;
 
 const Right = styled.div``;
 
 const Profile = styled.img`
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
+  width: 40px;
+  height: 40px;
 `;
 
 const DesktopGNB = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const { userInfo } = useGetUserInfo();
   const { characterImg } = useSetPosition(userInfo?.position || '', true);
+
+  const menus = [
+    { name: '동아리일정', path: '/calendar' },
+    { name: '게시판', path: '/board' },
+    { name: '출석', path: '/attendance' },
+    { name: '멤버', path: '/member' },
+    { name: '회비', path: '/dues' },
+  ];
+
   return (
     <Container>
       <Left>
-        <Logo src={logo} alt="Weeth" />
+        <Logo src={logo} alt="Weeth" onClick={() => navigate('/home')} />
         <Menu>
-          <li>동아리일정</li>
-          <li>게시판</li>
-          <li>출석</li>
-          <li>멤버</li>
-          <li>회비</li>
+          {menus.map((menu) => (
+            <MenuItem
+              key={menu.name}
+              active={location.pathname.startsWith(menu.path)}
+              onClick={() => navigate(menu.path)}
+            >
+              {menu.name}
+            </MenuItem>
+          ))}
         </Menu>
       </Left>
       <Right>
-        <Profile src={characterImg} alt="profile" />
+        <Profile
+          src={characterImg}
+          alt="profile"
+          onClick={() => navigate('/mypage')}
+        />
       </Right>
     </Container>
   );
