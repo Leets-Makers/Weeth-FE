0335cf519bdf52ec74476e1a8046aff94be57bf4
김지원 <jiwonkim0531@gachon.ac.kr>
2025-12-09 21:35:54 +0900

    #54 [FIX] 스터디로그 리스트 하위 스터디 및 주차 태그 디자인 토큰 적용

--- src/components/Board/WeekTag.tsx
+++ src/components/Board/WeekTag.tsx
@@ -1,4 +1,5 @@
 import theme from '@/styles/theme';
+import { colors } from '@/theme/designTokens';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -8,13 +9,13 @@ const Container = styled.div`
   border-radius: 24px;
   height: 20px;
   box-sizing: border-box;
-  background-color: ${theme.color.gray[18]};
+  background-color: ${colors.semantic.container.neutral};
   justify-content: center;
   align-items: center;
   font-size: 12px;
   font-family: ${theme.font.semiBold};
   font-size: 12px;
-  color: ${theme.color.gray[65]};
+  color: ${colors.semantic.text.alternative};
 `;
 
 const WeekTag = ({ week }: { week: number }) => {
