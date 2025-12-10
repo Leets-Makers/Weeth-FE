f120f71ba00c75d73919c4e181f7d5e7f0036237
김지원 <jiwonkim0531@gachon.ac.kr>
2025-12-10 20:51:59 +0900

    #54 [FIX] 파트게시판 글 수정 브레드크럼 추가

--- src/pages/board/part/PartEdit.tsx
+++ src/pages/board/part/PartEdit.tsx
@@ -1,14 +1,16 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import postBoardNotice from '@/api/postBoardNotice';
-import {
-  toastError,
-  toastInfo,
-  toastSuccess,
-} from '@/components/common/ToastMessage';
+// import postBoardNotice from '@/api/postBoardNotice';
+// import {
+//   toastError,
+//   toastInfo,
+//   toastSuccess,
+// } from '@/components/common/ToastMessage';
 import useGetBoardDetail from '@/api/useGetBoardDetail';
 import StudyWriteTemplate from '@/components/Board/StudyWriteTemplate';
-import getHeaderTitle from '@/utils/getHeaderTitle';
+import { BreadCrumContainer, CrumbButton } from '@/styles/breadCrum';
+import BreadcrumHomeIcon from '@/assets/images/ic_breadcrum_home.svg?react';
+import BreadcrumArrowRightIcon from '@/assets/images/ic_breadcrum_arrow_right.svg?react';
 
 export interface originFile {
   fileId: number;
@@ -32,8 +34,8 @@ const PartEdit = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [originFiles, setOriginFiles] = useState<originFile[]>([]);
 
-  const isTitleEmpty = title.trim() === '';
-  const isContentEmpty = content.trim() === '';
+  // const isTitleEmpty = title.trim() === '';
+  // const isContentEmpty = content.trim() === '';
   const numericPostId = postId ? parseInt(postId, 10) : 0;
 
   const { boardDetailInfo } = useGetBoardDetail(path, numericPostId);
@@ -47,79 +49,101 @@ const PartEdit = () => {
     setSelectedWeek(boardDetailInfo?.week ?? null);
   }, [boardDetailInfo]);
 
-  const onSave = async () => {
-    if (isTitleEmpty) {
-      toastInfo('제목을 입력해주세요.');
-      return;
-    }
-    if (isContentEmpty) {
-      toastInfo('내용을 입력해주세요.');
-      return;
-    }
+  // const onSave = async () => {
+  //   if (isTitleEmpty) {
+  //     toastInfo('제목을 입력해주세요.');
+  //     return;
+  //   }
+  //   if (isContentEmpty) {
+  //     toastInfo('내용을 입력해주세요.');
+  //     return;
+  //   }
 
-    try {
-      const postType = 'editPart';
+  //   try {
+  //     const postType = 'editPart';
 
-      if (title.length > 255) {
-        toastError('제목을 255자 이내로 작성해주세요.');
-        return;
-      }
+  //     if (title.length > 255) {
+  //       toastError('제목을 255자 이내로 작성해주세요.');
+  //       return;
+  //     }
 
-      if (content.length > 65000) {
-        toastError('내용을 65,000자 이내로 작성해주세요.');
-        return;
-      }
+  //     if (content.length > 65000) {
+  //       toastError('내용을 65,000자 이내로 작성해주세요.');
+  //       return;
+  //     }
 
-      await postBoardNotice({
-        originFiles,
-        files,
-        postData: {
-          title,
-          content,
-          studyName: selectedStudy || undefined,
-          week: selectedWeek || undefined,
-          cardinalNumber: selectedCardinal || undefined,
-          files: [],
-        },
-        postType,
-        id: numericPostId,
-      });
-      toastSuccess('게시글이 수정되었습니다.');
-      navigate(`/board/${category}/${part}`);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error: any) {
-      toastError(
-        path === 'board'
-          ? '게시글 작성 중 문제가 발생했습니다.'
-          : '공지사항 작성 중 문제가 발생했습니다.',
-      );
-    }
-  };
+  //     await postBoardNotice({
+  //       originFiles,
+  //       files,
+  //       postData: {
+  //         title,
+  //         content,
+  //         studyName: selectedStudy || undefined,
+  //         week: selectedWeek || undefined,
+  //         cardinalNumber: selectedCardinal || undefined,
+  //         files: [],
+  //       },
+  //       postType,
+  //       id: numericPostId,
+  //     });
+  //     toastSuccess('게시글이 수정되었습니다.');
+  //     navigate(`/board/${category}/${part}`);
+  //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //   } catch (error: any) {
+  //     toastError(
+  //       path === 'board'
+  //         ? '게시글 작성 중 문제가 발생했습니다.'
+  //         : '공지사항 작성 중 문제가 발생했습니다.',
+  //     );
+  //   }
+  // };
 
   if (!category || !part || !postId) {
     return <div>잘못된 경로입니다.</div>;
   }
 
+  const handleClickHome = () => {
+    navigate('/home');
+  };
+  const handleClickBoard = () => {
+    navigate('/board');
+  };
+  const handleClickPartBoard = () => {
+    navigate(`/board/study/${part}`);
+  };
+
   return (
-    <StudyWriteTemplate
-      category={category}
-      headerTitle={getHeaderTitle(category, part)}
-      selectedCardinal={selectedCardinal}
-      setSelectedCardinal={setSelectedCardinal}
-      selectedWeek={selectedWeek}
-      setSelectedWeek={setSelectedWeek}
-      selectedStudy={selectedStudy}
-      setSelectedStudy={setSelectedStudy}
-      title={title}
-      setTitle={setTitle}
-      content={content}
-      setContent={setContent}
-      files={files}
-      setFiles={setFiles}
-      originFiles={originFiles}
-      setOriginFiles={setOriginFiles}
-      onSave={onSave}
-    />
+    <>
+      <BreadCrumContainer>
+        <BreadcrumHomeIcon onClick={handleClickHome} />
+        <BreadcrumArrowRightIcon />
+        <CrumbButton onClick={handleClickBoard}>게시판</CrumbButton>
+        <BreadcrumArrowRightIcon />
+        <CrumbButton onClick={handleClickPartBoard}>
+          {part} 파트게시판
+        </CrumbButton>
+        <BreadcrumArrowRightIcon />
+        글쓰기 수정
+      </BreadCrumContainer>
+      <StudyWriteTemplate
+        category={category}
+        selectedCardinal={selectedCardinal}
+        setSelectedCardinal={setSelectedCardinal}
+        selectedWeek={selectedWeek}
+        setSelectedWeek={setSelectedWeek}
+        selectedStudy={selectedStudy}
+        setSelectedStudy={setSelectedStudy}
+        title={title}
+        setTitle={setTitle}
+        content={content}
+        setContent={setContent}
+        files={files}
+        setFiles={setFiles}
+        originFiles={originFiles}
+        setOriginFiles={setOriginFiles}
+        // onSave={onSave}
+      />
+    </>
   );
 };
 
