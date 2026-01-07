e70c9639a5cbda034b9ddfaec0f2f21b455a0a4f
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-07 10:25:17 +0900

    #54 [FEAT] 브레드크럼 컴포넌트 분리

--- src/styles/breadCrum/index.ts
+++ src/styles/breadCrum/index.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import typography from '@/theme/typography';
 import { colors, units } from '@/theme/designTokens';
 
-export const BreadCrumContainer = styled.div`
+export const BreadCrumContainer = styled.div<{ $hasTitle?: boolean }>`
   display: flex;
   ${typography.Caption1};
   color: ${colors.semantic.text.alternative};
-  padding: ${units.padding['450']}px;
+  ${({ $hasTitle }) =>
+    !$hasTitle ? `padding: ${units.padding['450']}px;` : ''}
 `;
 
 export const CrumbButton = styled.div`
