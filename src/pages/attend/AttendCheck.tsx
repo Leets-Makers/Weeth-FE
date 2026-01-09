3c1a4caa77dce5d97e14b70a0ecfc8a49e346871
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-09 17:11:25 +0900

    #77 [FEAT] 출석 조회 브레드크럼 추가

--- src/pages/attend/AttendCheck.tsx
+++ src/pages/attend/AttendCheck.tsx
@@ -1,12 +1,20 @@
 import AttendCheckMain from '@/components/AttendCheck/AttendCheckMain';
 import useCustomBack from '@/hooks/useCustomBack';
 import React from 'react';
+import Breadcrumb from '@/components/common/Breadcrumb';
 import { AttendContainer } from './Attendance';
 
 const AttendCheck: React.FC = () => {
   useCustomBack('/attendance');
   return (
     <AttendContainer>
+      <Breadcrumb
+        items={[
+          { label: '출석', path: '/attendance' },
+          { label: '출석 조회', path: '/attendCheck' },
+        ]}
+        hasTitle
+      />
       <AttendCheckMain />
     </AttendContainer>
   );
