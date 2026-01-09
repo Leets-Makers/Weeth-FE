6f99d9137ee1340b773c2f042a92d12169d007e5
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-09 17:11:11 +0900

    #77 [FEAT] 영수증 브레드크럼 추가

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -2,6 +2,8 @@ import ReceiptList from '@/components/Receipt/ReceiptList';
 import useCustomBack from '@/hooks/useCustomBack';
 
 import { StyledDues } from '@/styles/dues/Dues.styled';
+import Breadcrumb from '@/components/common/Breadcrumb';
+import { DuesHeaderContainer } from '@/components/Dues/DuesTitle';
 import { PageHeader } from './attend/Penalty';
 
 const Receipt: React.FC = () => {
@@ -9,7 +11,16 @@ const Receipt: React.FC = () => {
 
   return (
     <StyledDues>
-      <PageHeader>영수증</PageHeader>
+      <DuesHeaderContainer>
+        <Breadcrumb
+          items={[
+            { label: '회비', path: '/dues' },
+            { label: '영수증', path: '/receipt' },
+          ]}
+          hasTitle
+        />
+        <PageHeader>영수증</PageHeader>
+      </DuesHeaderContainer>
       <ReceiptList />
     </StyledDues>
   );
