9b5b10288849b24b3b75b76cef238101da9e96d1
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-04 15:27:41 +0900

    #109 [Refactor] 공지 작성에 usePostBoard 적용

--- src/pages/board/notices/NoticePost.tsx
+++ src/pages/board/notices/NoticePost.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
-import postBoardNotice from '@/api/postBoardNotice';
+import usePostBoard from '@/hooks/mutation/usePostBoard';
 import { toastError, toastInfo } from '@/components/common/ToastMessage';
 import NoticeWrite from '@/components/Board/NoticeWrite';
 import Breadcrumb from '@/components/common/Breadcrumb';
@@ -23,7 +23,21 @@ const NoticePost = () => {
   const isContentEmpty = content.trim() === '';
   const numericPostId = postId ? parseInt(postId, 10) : 0;
 
-  const handleClickButton = async () => {
+  const postBoardMutation = usePostBoard({
+    onSuccess: () => {
+      navigate('/board/notices');
+    },
+    onError: (message) => {
+      toastError(
+        message ??
+          (path === 'board'
+            ? '게시글 작성 중 문제가 발생했습니다.'
+            : '공지사항 작성 중 문제가 발생했습니다.'),
+      );
+    },
+  });
+
+  const handleClickButton = () => {
     if (isTitleEmpty) {
       toastInfo('제목을 입력해주세요.');
       return;
@@ -33,42 +47,28 @@ const NoticePost = () => {
       return;
     }
 
-    try {
-      // 요청 타입 결정
-      const postType = path === 'board' ? 'postBoard' : 'postNotice';
+    const postType = path === 'board' ? 'postBoard' : 'postNotice';
 
-      if (title.length > 255) {
-        toastError('제목을 255자 이내로 작성해주세요.');
-        return;
-      }
-
-      if (content.length > 65000) {
-        toastError('내용을 65,000자 이내로 작성해주세요.');
-        return;
-      }
-
-      // 서버 요청
-      await postBoardNotice({
-        files,
-        postData: {
-          title,
-          content,
-          files: [],
-        },
-        postType,
-        id: numericPostId,
-      });
-
-      // 게시글 생성 후 이동
-      navigate('/board/notices');
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error: any) {
-      toastError(
-        path === 'board'
-          ? '게시글 작성 중 문제가 발생했습니다.'
-          : '공지사항 작성 중 문제가 발생했습니다.',
-      );
+    if (title.length > 255) {
+      toastError('제목을 255자 이내로 작성해주세요.');
+      return;
     }
+
+    if (content.length > 65000) {
+      toastError('내용을 65,000자 이내로 작성해주세요.');
+      return;
+    }
+
+    postBoardMutation.mutate({
+      files,
+      postData: {
+        title,
+        content,
+        files: [],
+      },
+      postType,
+      id: numericPostId,
+    });
   };
 
   return (
