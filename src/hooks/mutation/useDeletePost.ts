160a49f502cbbaf3f69fa3dfca529b8fe92130aa
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-04 15:22:18 +0900

    #109 [Feature] 게시글 삭제 mutation 훅 추가

---
+++ src/hooks/mutation/useDeletePost.ts
@@ -0,0 +1,49 @@
+import deletePost from '@/api/board/deletePost';
+import { BOARD_QUERY_KEYS } from '@/constants/queryKeys';
+import { useMutationCallback } from '@/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+interface DeletePostParams {
+  postId: number;
+  path: string;
+}
+
+const useDeletePost = (callbacks?: useMutationCallback) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ postId, path }: DeletePostParams) =>
+      deletePost(postId, path),
+    onMutate: () => {
+      callbacks?.onMutate?.();
+    },
+    onSuccess: async (_, variables) => {
+      if (variables.path === 'notices') {
+        await queryClient.invalidateQueries({
+          queryKey: BOARD_QUERY_KEYS.notices.all,
+        });
+        await queryClient.invalidateQueries({
+          queryKey: BOARD_QUERY_KEYS.notices.recent,
+        });
+      } else {
+        await queryClient.invalidateQueries({
+          queryKey: ['board', 'part'],
+        });
+      }
+      callbacks?.onSuccess?.();
+    },
+    onError: (error) => {
+      let message = '게시글 삭제에 실패했습니다.';
+      if (error instanceof AxiosError) {
+        message = error.response?.data?.message ?? message;
+      }
+      callbacks?.onError?.(message);
+    },
+    onSettled: () => {
+      callbacks?.onSettled?.();
+    },
+  });
+};
+
+export default useDeletePost;
