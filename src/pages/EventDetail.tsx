a03ad45f99a315c7307fb712141de891d20fa135
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-05 14:24:32 +0900

    fix: eventDetail에 로딩상태 추가

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -6,6 +6,7 @@ import useCustomBack from '@/hooks/useCustomBack';
 import { useLocation, useParams } from 'react-router-dom';
 import { CURRENT_MONTH, CURRENT_YEAR } from '@/constants/dateConstants';
 import useUserData from '@/hooks/queries/useUserData';
+import Loading from '@/components/common/Loading';
 
 export type { EventDetailData } from '@/api/event/getEventInfo';
 
@@ -19,12 +20,15 @@ const EventDetail = () => {
   const { id, type } = useParams();
   const { data: userInfo } = useUserData();
   const isAdmin = userInfo?.role === 'ADMIN';
-  const { data: eventDetailData, error } = useEventInfo(type, id);
+  const { data: eventDetailData, error, isLoading } = useEventInfo(type, id);
 
+  if (isLoading) return <Loading />;
   if (!eventDetailData)
     return (
       <S.Error>
-        {error ? error.message : '데이터를 불러오지 못했습니다.'}
+        {error instanceof Error
+          ? error.message
+          : '데이터를 불러오지 못했습니다.'}
       </S.Error>
     );
 
