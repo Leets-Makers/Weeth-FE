0e1ec900d01004f2a7ec8d625dca9761eed4a3a2
JIN921 <wlslwlsl808@gmail.com>
2026-02-07 00:08:09 +0900

    fix: 홈 구성요소 배치 변경

--- src/styles/home/HomeMain.styled.ts
+++ src/styles/home/HomeMain.styled.ts
@@ -18,9 +18,9 @@ export const GridContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: auto auto auto;
   grid-template-areas:
-    'calendar penalty'
-    'board member'
-    'board dues';
+    'board penalty'
+    'calendar member'
+    'calendar dues';
 
   /* 태블릿/PC (새로운 레이아웃 - 3열) */
   @media screen and (min-width: 768px) {
