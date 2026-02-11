8cd3c2a41bf7084c80ed04c91abce8fd7aeabd31
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-11 16:04:57 +0900

    fix: layout 프리티어 설정

--- src/Layout/layout.tsx
+++ src/Layout/layout.tsx
@@ -6,24 +6,17 @@ import { ThemeProvider } from 'styled-components';
 import { buildTheme } from '@/theme/buildTheme';
 import { baseThemeMap } from '@/theme/baseThemeMap';
 
-
 const Layout = () => {
-   // admin은 항상 light 모드
-   const adminTheme = useMemo(
-    () => buildTheme(baseThemeMap, 'light'),
-    [],
-  );
+  // admin은 항상 light 모드
+  const adminTheme = useMemo(() => buildTheme(baseThemeMap, 'light'), []);
 
-  
   return (
-    <>
-      <ThemeProvider theme={adminTheme}>
-        <ScrollToTop />
-        <Suspense fallback={<DelayedFallback delay={300} />}>
-          <Outlet />
-        </Suspense>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={adminTheme}>
+      <ScrollToTop />
+      <Suspense fallback={<DelayedFallback delay={300} />}>
+        <Outlet />
+      </Suspense>
+    </ThemeProvider>
   );
 };
 
